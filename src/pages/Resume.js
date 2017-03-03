2d1f6302183125b000341ab58b5d1d9bedfb4eff
Andrew Yeonsoo Han <drewhan90@gmail.com>
2017-03-03 00:04:03 -0800

    Resume Update

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -25,104 +25,112 @@ class Resume extends Component {
             <h1>Professional <span className="Resume-color">RESUME</span></h1>
           </header>
           <section className="Resume-download">
-            <a href={resumePNG} download>download</a>
+            <a href={resumePNG} download></a>
           </section>
+          {/* HTML RESUME STARTS HERE */}
           <section className="Resume-htmlResume">
             <header className="htmlResume-header">
+              <div className="htmlResume-brand">
+                <img className="htmlResume-logo App-logo" src={logo} alt="logo" />
+                <h1 className="noMarginPadding">andrew han</h1>
+                <h3 className="noMarginPadding">web developer & interactive desiner</h3>
+              </div>
               <span className="htmlResume-headerBG"></span>
-              <img className="htmlResume-logo App-logo" src={logo} alt="logo" />
-              <h1>andrew han</h1>
-              <h3>web developer & interactive desiner</h3>
             </header>
-            <div className="Resume-content">
+            <div className="htmlResume-content">
               {/* RESUME: CONTACT */}
-              <div className="Resume-contact">
-                <ul>
-                  <li className="contact-item">
+              <div className="htmlResume-contact">
+                <ul className="contactResume-list Resume-list">
+                  <li>
                     <p>(778) 681 - 1317</p>
+                    <span className="icon-Phone"></span>
                   </li>
-                  <li className="contact-item"><a href="">
+                  <li><a href="mailto:drewhan90@gmail.com">
                     <p>drewhan90@gmail.com</p>
+                    <span className="icon-Mail"></span>
                   </a></li>
-                  <li className="contact-item"><a href="">
+                  <li><a href="www.andrewyhan.com">
                     <p>www.andrewyhan.com</p>
+                    <span className="icon-www"></span>
                   </a></li>
-                  <li className="contact-item"><a href="">
+                  <li><a href="github.com/drewhan90">
                     <p>github.com/drewhan90</p>
+                    <span className="icon-GitHub"></span>
                   </a></li>
-                  <li className="contact-item"><a href="">
+                  <li><a href="www.linkedin.com/in/andrewyhan">
                     <p>www.linkedin.com/in/andrewyhan</p>
+                    <span className="icon-LinkedIn"></span>
                   </a></li>
                 </ul>
               </div>
               {/* RESUME: EXPERIENCE */}
-              <div className="Resume-experience Resume-container">
+              <div className="htmlResume-exp htmlResume-box">
                 <header>
                   <h2>experience</h2>
                 </header>
-                <ul>
-                  <li className="experience-item">
-                    <h3 className="experience-title">Freelance Web Developer</h3>
-                    <p className="experience-sub">Andrew Y Han, 2016 - current, Vancouver</p>
-                    <ul className="experience-desc">
-                      <li><p>Develop websites for independent clients</p></li>
-                      <li><p>Manage budget and timelines</p></li>
+                <ul className="Resume-list">
+                  <li className="exp-item">
+                    <h3 className="noMarginPadding">Freelance Web Developer</h3>
+                    <p className="exp-location noMarginPadding">Andrew Y Han, 2016 - current, Vancouver</p>
+                    <ul className="exp-desc">
+                      <li>Develop websites for independent clients.</li>
+                      <li>Manage budget and timelines.</li>
                     </ul>
                   </li>
-                  <li className="experience-item">
-                    <h3 className="experience-title">Business English Coach</h3>
-                    <p className="experience-sub">HelloCafe, 2015, Seoul, South Korea</p>
-                    <ul className="experience-desc">
-                      <li><p>Coach English business communication skills to Korean corporate employees</p></li>
+                  <li className="exp-item">
+                    <h3 className="noMarginPadding">Business English Coach</h3>
+                    <p className="exp-location noMarginPadding">HelloCafe, 2015, Seoul, South Korea</p>
+                    <ul className="exp-desc">
+                      <li>Coach English business communication skills to Korean corporate employees</li>
                     </ul>
                   </li>
-                  <li className="experience-item">
-                    <h3 className="experience-title">Event Journalist</h3>
-                    <p className="experience-sub">WorknPlay, 2015, Seoul, South Korea</p>
-                    <ul className="experience-desc">
-                      <li><p>Research and publish articles</p></li>
-                      <li><p>Design a user friendly website for WorknPlay</p></li>
+                  <li className="exp-item">
+                    <h3 className="noMarginPadding">Event Journalist</h3>
+                    <p className="exp-location noMarginPadding">WorknPlay, 2015, Seoul, South Korea</p>
+                    <ul className="exp-desc">
+                      <li>Research and publish articles.</li>
+                      <li>Design a user friendly website for WorknPlay.</li>
                     </ul>
                   </li>
-                  <li className="experience-item">
-                    <h3 className="experience-title">freelance web developer</h3>
-                    <p className="experience-sub">You & Us, 2014 - 2015, Seoul, South Korea</p>
-                    <ul className="experience-desc">
-                      <li><p>Retail international high-end carpet and fabric brands to the Korean Market</p></li>
+                  <li className="exp-item">
+                    <h3 className="noMarginPadding">freelance web developer</h3>
+                    <p className="exp-location noMarginPadding">You & Us, 2014 - 2015, Seoul, South Korea</p>
+                    <ul className="exp-desc">
+                      <li>Retail international high-end carpet and fabric brands to the Korean Market</li>
                     </ul>
                   </li>
                 </ul>
               </div>
               {/* RESUME: SKILLS */}
-              <div className="Resume-skills Resume-container">
+              <div className="htmlResume-skills htmlResume-box">
                 <div className="row">
                   <div className="skills-web skills-item col-6">
                     <header>
                       <h2>web skills</h2>
                     </header>
-                    <ul>
-                      <li><p>HTML + CSS</p></li>
-                      <li><p>Javascript + Jquery</p></li>
-                      <li><p>PHP</p></li>
-                      <li><p>ReactJS + Redux</p></li>
-                      <li><p>AngularJS</p></li>
-                      <li><p>NodeJS</p></li>
-                      <li><p>Wordpress</p></li>
-                      <li><p>Git</p></li>
+                    <ul className="Resume-list">
+                      <li>HTML + CSS</li>
+                      <li>Javascript + Jquery</li>
+                      <li>PHP</li>
+                      <li>ReactJS + Redux</li>
+                      <li>AngularJS</li>
+                      <li>NodeJS</li>
+                      <li>Wordpress</li>
+                      <li>Git</li>
                     </ul>
                   </div>
                   <div className="skills-design skills-item col-6">
                     <header>
                       <h2>design skills</h2>
                     </header>
-                    <ul>
-                      <li><p>Photoshop</p></li>
-                      <li><p>Illustator</p></li>
-                      <li><p>InDesign</p></li>
-                      <li><p>After Effects</p></li>
-                      <li><p>Premiere</p></li>
-                      <li><p>Sketch (App)</p></li>
-                      <li><p>AxureRP</p></li>
+                    <ul className="Resume-list">
+                      <li>Photoshop</li>
+                      <li>Illustator</li>
+                      <li>InDesign</li>
+                      <li>After Effects</li>
+                      <li>Premiere</li>
+                      <li>Sketch (App)</li>
+                      <li>AxureRP</li>
                     </ul>
                   </div>
                 </div>
@@ -131,25 +139,41 @@ class Resume extends Component {
                     <header>
                       <h2>marketing skills</h2>
                     </header>
-                    <ul>
-                      <li><p>Psychology</p></li>
-                      <li><p>SEO</p></li>
-                      <li><p>Project Management</p></li>
-                      <li><p>Google Analytics</p></li>
-                      <li><p>Research</p></li>
+                    <ul className="Resume-list">
+                      <li>Psychology</li>
+                      <li>SEO></li>
+                      <li>Project Management</li>
+                      <li>Google Analytics</li>
+                      <li>Research</li>
                     </ul>
                   </div>
                   <div className="skills-language skills-item col-6">
                     <header>
                       <h2>language skills</h2>
                     </header>
-                    <ul>
-                      <li><p>English</p></li>
-                      <li><p>Korean</p></li>
+                    <ul className="Resume-list">
+                      <li>English</li>
+                      <li>Korean</li>
                     </ul>
                   </div>
                 </div>
               </div>
+            </div>
+            {/* RESUME: EDUCATION */}
+            <div className="htmlResume-edu htmlResume-box">
+              <header>
+                <h2>education</h2>
+              </header>
+              <ul className="Resume-list">
+                <li className="edu-item">
+                  <h3 className="noMarginPadding">Diploma, Web Development & Interactive Design</h3>
+                  <p className="edu-location noMarginPadding">Vancouver Institue of Media Arts, 2017</p>
+                </li>
+                <li className="edu-item">
+                  <h3 className="noMarginPadding">Bachelor of Arts, Psychology</h3>
+                  <p className="edu-location noMarginPadding">University of British Columbia, 2015</p>
+                </li>
+              </ul>
             </div>
           </section>
         </div>
